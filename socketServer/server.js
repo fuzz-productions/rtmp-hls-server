e7d3f8296d2af2d2c9d072dcd37cc4d7888b26fb
Nick Trienens <nick@fuzz.pro>
2020-10-23 15:21:11 -0600

    [updated nginxconf] no more record

--- socketServer/server.js
+++ socketServer/server.js
@@ -23,16 +23,8 @@ server.use( restify.plugins.bodyParser() );
 server.use( cors.preflight );
 server.use( cors.actual );
 
-let debug = process.env.DEBUG || false,
-    api_key = process.env.API_KEY;
 
-// Start-up
-// console.log( colors.bold( '%s v%s by %s / %s' ), pkg.config.app_name, pkg.version, pkg.author.name, pkg.author.url );
-// console.log( 'License: %s / %s', pkg.license, pkg.config.links.license );
-// console.log( 'GitHub: ' + pkg.homepage );
-// console.log( 'Docker: ' + pkg.config.links.docker );
-
-if( typeof api_key === 'undefined' || !api_key.length ) console.warn( colors.brightYellow( '[warn] API_KEY is not defined.' ) );
+const activeUsers = new Set();
 
 // Static routing
 // server.get( '/', ( req, res, next ) => { next
@@ -45,39 +37,33 @@ server.get( '/', restify.plugins.serveStatic( { file: 'demo.html',  directory: _
 server.get( '/favicon.ico', restify.plugins.serveStatic( { directory: __dirname + '/public' } ) );
 
 // Relay messages to connected clients
-server.post( '/socket/:socket', function( req, res, next ) {
-
-    if( typeof api_key === 'undefined' || !api_key.length || ( typeof req.query.api_key === 'undefined' || api_key !== req.query.api_key ) ) {
-        if( debug ) console.log( colors.red( '[error] Invalid API key from', req.connection.remoteAddress ) );
-        next( new errors.InvalidCredentialsError( 'Unauthorized' ) );
-    } else {
-        if( debug ) console.log( '[msg]', req.body );
-        io.emit( req.params.socket, req.body );
+server.post( '/socket/', function( req, res, next ) {
+        console.log( '[msg]', req.body );
+        //io.emit( req.params.socket, req.body );
         res.send( req.body );
+        io.emit("any", req.body);
         next();
-    }
 });
 
-// io.sockets.on('connection', function(socket) {
-//     console.log("welcome");
-//     //io.emit( socket, "Welcome" );
-
-//     socket.on('disconnect', function () {
-//       console.log("disconnect");
-      
-//     });
-//   });
-
-
-const activeUsers = new Set();
-
-io.on("connection", function (socket) {
+io.on("connect", function (socket) {
   console.log("Made socket connection");
+  let counter = 0;
+  setInterval(() => {
+    ++counter
+    console.log("sending", counter);
+    socket.emit('hello', counter);
+
+  }, 1000);
+  
+  io.emit("AssignNumber", activeUsers.count);
+  console.log("AssignNumber", activeUsers.count);
 
   socket.on("new user", function (data) {
+    console.log(data);
     socket.userId = data;
     activeUsers.add(data);
     io.emit("new user", [...activeUsers]);
+    socket.broadcast.emit("typing", data);
   });
 
   socket.on("disconnect", () => {
